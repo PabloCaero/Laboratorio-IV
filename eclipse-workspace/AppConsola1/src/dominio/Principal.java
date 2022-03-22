b0bd965821a694cae1aa6a64d5ff658953b53492
Pablo Ezequiel Caero <pablo_ieme@hotmail.com>
2022-03-22 18:23:04 -0300

    1 - Clases - Encapsulamiento - Método toString()

---
+++ eclipse-workspace/AppConsola1/src/dominio/Principal.java
@@ -0,0 +1,17 @@
+package dominio;
+
+public class Principal {
+	public static void main(String[] args) {
+		Articulo art = new Articulo(); //Instancia del objeto por el constructor vacío
+		
+		art.setId(2);
+		art.setNombre("Articulo 2");
+		
+		//Para mostrar lo que tiene el objeto
+		//System.out.println("ID: " + art.getId()+" - NOMBRE: " + art.getNombre());
+		
+		//Para mostrar con el método ToString()
+		
+		System.out.println(art.toString());
+	}
+}
